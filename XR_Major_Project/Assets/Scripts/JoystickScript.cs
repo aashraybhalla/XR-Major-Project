6881a7d1ba8f6ed59b138bd77146e0380800dac3
Taneesha Kalra <49729371+taneeshaa@users.noreply.github.com>
2024-12-17 21:07:13 +0530

    Joystick

---
+++ XR_Major_Project/Assets/Scripts/JoystickScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+public class JoystickScript : MonoBehaviour
+{
+    public XRJoystick joystick;
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(joystick.value.x, 0f, joystick.value.y)* moveSpeed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
