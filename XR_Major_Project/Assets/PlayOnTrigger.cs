4cf780613488dc8d525a19421de62bcae4f76ce1
Taneesha Kalra <49729371+taneeshaa@users.noreply.github.com>
2024-12-17 17:27:36 +0530

    ct scan fixes

--- XR_Major_Project/Assets/PlayOnTrigger.cs
+++ XR_Major_Project/Assets/PlayOnTrigger.cs
@@ -6,12 +6,15 @@ public class PlayOnTrigger : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private string triggerName;
+    [SerializeField] private GameObject patientBody;
+    
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             animator.SetTrigger(triggerName);
+            patientBody.SetActive(false);
         }
     }
 }
