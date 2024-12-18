7fc3ae1531a3c11320fbeca3ec7a9410dd3f7d77
AASHRAY BHALLA <aashraybhalla81@gmail.com>
2024-12-18 13:01:45 +0530

    Main UI updated

--- XR_Major_Project/Assets/Scripts/LoadScene.cs
+++ XR_Major_Project/Assets/Scripts/LoadScene.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
+    public GameObject descriptionText;
    
     public void LoadThisScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowDescription()
+    {
+        descriptionText.SetActive(true);
     }
 }
